539989e5a9ca0b7dc271bf5f076018e62ec972d2
Fahad Guled <fahad.guled@yahoo.com>
2023-07-19 14:52:08 -0700

    added matchup_id, points, and starters for each owner in the players map.

--- stats.js
+++ stats.js
@@ -5,8 +5,10 @@ import { OpenAI } from "langchain/llms/openai";
 import { PromptTemplate } from "langchain/prompts";
 import { LLMChain } from "langchain/chains";
 import axios from "axios";
+import { match } from "assert";
 
 const map = new Map();
+const duplicateMatchup = new Map();
 
 const getUsers = async () => {
   const response = await axios.get(
@@ -50,53 +52,81 @@ const getRosters = async () => {
   console.log("rosters completed");
 };
 
-let matchupData;
+let week;
 const getMatchups = async () => {
   const response = await axios.get(
     "https://api.sleeper.app/v1/league/864448469199347712/matchups/1"
   );
-  matchupData = response.data;
+  week = response.data;
 };
 
 getUsers()
   .then(getRosters)
   .then(getMatchups)
-  .then(() => {
+  .then(async () => {
     console.log("All promises resolved");
-    console.log(map);
+    //console.log(map);
+    //when map is populated with name, avatar, and roster_id now we can access matchups like we are doing below
     [...map.values()].map((player) => {
-      for (let week of matchupData) {
-        if (player.roster_id === week.roster_id) {
-          let matchup = matchupData.filter(
-            (team) => team.matchup_id === player.matchup_id
+      for (let playerInfo of week) {
+        //console.log(playerInfo);
+        if (
+          player.roster_id === playerInfo.roster_id &&
+          //1) refrain from any duplicate matchups being logged
+          !duplicateMatchup.has(playerInfo.matchup_id)
+        ) {
+          let matchup = week.filter(
+            (team) => team.matchup_id === playerInfo.matchup_id
           );
-          console.log(matchup);
+
+          let team1 = [...map.values()].find(
+            (team) => team.roster_id === matchup[0].roster_id
+          );
+          let team2 = [...map.values()].find(
+            (team) => team.roster_id === matchup[1].roster_id
+          );
+          //2) refrain from any duplicate matchups being logged.
+          // duplicateMatchup.set(matchup[0].matchup_id, team1);
+
+          if (player.roster_id === matchup[0].roster_id) {
+            player.matchup_id = matchup[0].matchup_id;
+            player.team_points = matchup[0].points;
+            player.starters = matchup[0].starters;
+          } else if (player.roster_id === matchup[1].roster_id) {
+            player.matchup_id = matchup[1].matchup_id;
+            player.team_points = matchup[1].points;
+            player.starters = matchup[1].starters;
+          }
         }
       }
     });
+    //langchain AI work
+    const model = new OpenAI({
+      temperature: 0,
+    });
+
+    const template =
+      "give me only 1 user that starts with the letter a {data} ";
+
+    const promptTemplate = new PromptTemplate({
+      template: template,
+      inputVariables: ["data"],
+    });
+
+    const chain = new LLMChain({
+      llm: model,
+      prompt: promptTemplate,
+    });
+
+    const obj = Object.fromEntries(map);
+    const jsonString = JSON.stringify(obj);
+
+    const response = await chain.call({
+      data: map,
+    });
+
+    console.log(response);
   })
   .catch((error) => {
     console.error("At least one promise was rejected:", error);
   });
-
-// const model = new OpenAI({
-//   temperature: 0,
-// });
-
-// const template = "give me only 1 user that starts with the letter a {users} ";
-
-// const promptTemplate = new PromptTemplate({
-//   template: template,
-//   inputVariables: ["users", "rosters", "matchups"],
-// });
-
-// const chain = new LLMChain({
-//   llm: model,
-//   prompt: promptTemplate,
-// });
-
-// const response = await chain.call({
-//   users: usersRes,
-//   rosters: rostersRes,
-//   matchups: matchupRes,
-// });
